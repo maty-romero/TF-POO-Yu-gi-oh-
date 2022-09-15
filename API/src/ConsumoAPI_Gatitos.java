141768a6cc9bc18f6c2837225e2b19baf9bf0c0e
maty-romero <110309525+maty-romero@users.noreply.github.com>
2022-09-14 21:03:20 -0300

    avance

--- API/src/ConsumoAPI_Gatitos.java
+++ API/src/ConsumoAPI_Gatitos.java
@@ -2,6 +2,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 public class ConsumoAPI_Gatitos {
 
 	public static void main(String[] args) {
@@ -15,9 +19,9 @@ public class ConsumoAPI_Gatitos {
 		Si es info sensible se utiliza POST 
 		 */
 		
-		
+		//
 		try {
-			URL url = new URL("https://api.thecatapi.com/v1/images/search");
+			URL url = new URL("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician");
 			//conexion a la url 
 			HttpURLConnection conx = (HttpURLConnection) url.openConnection();
 			//solicitar la conexion a traves de GET 
@@ -33,11 +37,13 @@ public class ConsumoAPI_Gatitos {
 			}else { //continuamos con el flujo normal
 				
 				// Se lee un flujo de datos con un Scanner 
+//				String infoString = ""; 
 				StringBuilder infoString = new StringBuilder();
 				Scanner entrada = new Scanner(url.openStream()); //entra el flujo de datos al objeto Scanner hasta que 
 				
 				while(entrada.hasNext()) { //mientras haya informacion en el flujo de datos..
 					infoString.append(entrada.nextLine()); //obtengo la siguiente linea
+//					infoString += entrada.nextLine();
 				}
 				
 				entrada.close(); //cerramos flujo de datos 
@@ -45,6 +51,16 @@ public class ConsumoAPI_Gatitos {
 				//Se imprime en consola la informacion recepcionada
 				
 				System.out.println(infoString);
+				
+				//tenemos la info en un Json array 
+				JSONArray jArray = new JSONArray(infoString.toString());
+				
+				//accedemos a un json array. 
+				JSONObject jsObject = jArray.getJSONObject(0);
+				System.out.println(jsObject.getString("id")); //obtenemos el valor de la clave pasada por parametro
+				
+//				JsonObject jsonObject = new JsonParser().parse(infoString).getAsJsonObject();
+//				Assert.assertTrue(jsonObject.get("name").getAsString().equals("Baeldung"));
 			}
 			
 			
