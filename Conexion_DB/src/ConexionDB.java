d508166d45bce50e16781bc02d69b101a1057b31
maty-romero <110309525+maty-romero@users.noreply.github.com>
2022-10-19 11:07:13 -0300

    conexion DB

--- Conexion_DB/src/ConexionDB.java
+++ Conexion_DB/src/ConexionDB.java
@@ -11,30 +11,26 @@ public class ConexionDB {
 	 * Conexion a una BD Postgresql utilizando Singleton
 	 */
 
-	private static ConexionDB instance;
+	private static ConexionDB db = null; 
 	private Connection connection;
 	private String url = "jdbc:postgresql://localhost/YUGIOH_DB";
 	private String username = "postgres";
 	private String password = "gatos123";
-	
-	private PreparedStatement ps = null; 
-	private ResultSet rs = null; 
-
+	 
 	private ConexionDB() {
 		try {
-			Class.forName("org.postgresql.Driver");
-			this.connection = DriverManager.getConnection(url, username, password);
+			this.setConnection(DriverManager.getConnection(url, username, password));
 			System.out.println("Conexion establecida con la BD.");
 			
-		} catch (ClassNotFoundException e) {
-			System.out.println("Conexion con BD fallida: " + e.getMessage());
 		} catch(SQLException e) {
-			System.out.println("Consulta SQL Fallida. " + e.getMessage());
-			e.printStackTrace();
+			System.out.println("Conexion con BD fallida. " + e.getMessage());
 		}
 	}
 	
+	//se devuelve ResultSet dado un query para mas flexibilidad 
 	public ResultSet query(String query) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
 			ps = getConnection().prepareStatement(query);
 			rs = ps.executeQuery();
@@ -42,54 +38,23 @@ public class ConexionDB {
 			e.printStackTrace();
 		}
 		return rs; 
+	}
+	
+	//retorno de una sola instancia - Singleton
+	public static ConexionDB getInstance() throws SQLException {
+		return db != null? db: new ConexionDB(); 
+	}
 
-	}
-	
-	//Imprime en consola el ResultSet
-	public void ImpresionCompleta(ResultSet rs) {
-		try {
-			while (rs.next()) {
-				System.out.println(rs.getInt("id_carta"));
-				System.out.println(rs.getString("nombre"));
-				System.out.println(rs.getString("descripcion"));
-				System.out.println(rs.getString("imagen_url"));
-				System.out.println(rs.getString("ataque"));
-				System.out.println(rs.getString("defensa"));
-
-				System.out.println("");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public CartaMonstruo getInstanceMonstruo(ResultSet rs) {
-		
-		
-		return new CartaMonstruo(); 
-	}
-	
-	
-	
-	
 	public Connection getConnection() {
 		return connection;
 	}
 
-	public static ConexionDB getInstance() throws SQLException {
-		if (instance == null) {
-			instance = new ConexionDB();
-
-		} else if (instance.getConnection().isClosed()) {
-			instance = new ConexionDB();
-		}
-
-		return instance;
+	private void setConnection(Connection connection) {
+		this.connection = connection;
 	}
 
 	
-
+	
   }
 
 	
--- Conexion_DB/src/Main.java
+++ Conexion_DB/src/Main.java
@@ -5,13 +5,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
+		
 		try {
-			ConexionDB db = ConexionDB.getInstance();
+//			ConexionDB db = ConexionDB.getInstance();
+//			
+//			ConexionDB.getInstance().ImpresionCompleta(
+//					ConexionDB.getInstance().query("SELECT * FROM monstruos")
+//					);
+			String query = "SELECT * from "; //para obtener las demas DB 
 			
-			ConexionDB.getInstance().ImpresionCompleta(
-					ConexionDB.getInstance().query("SELECT * FROM monstruos")
-					);
+			ResultSet rs = ConexionDB.getInstance().query(query + "monstruos"); 
+			System.out.println(rs.getInt("id_carta"));
 			
+//			while(rs != null) { //recorre fila por fila asiganando
+//				
+//				Integer id = rs.getInt("id_carta");
+//				String nombre = rs.getString("nombre");
+//				String descripcion = rs.getString("descripcion");
+//				String url = rs.getString("imagen_url");
+//				Integer ataque = rs.getInt("ataque");
+//				Integer defensa = rs.getInt("defensa");
+//				 
+//				
+//			}
 
 			
 		} catch (SQLException e) {
