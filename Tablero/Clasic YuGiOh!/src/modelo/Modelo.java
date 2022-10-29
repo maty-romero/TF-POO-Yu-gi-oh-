7742ac8f925022ce316cc82a66ffb166da707d1b
maty-romero <110309525+maty-romero@users.noreply.github.com>
2022-10-28 21:22:06 -0300

    Revert "Merge branch 'main' of https://github.com/maty-romero/TF-POO-Yu-gi-oh-"
    This reverts commit 7882e2d8327e309e44d7fdde273c633b50f78f5a, reversing
    changes made to 71903eb8ff541b923a0dcfcdc919f974e464679d.

--- Tablero/Clasic YuGiOh!/src/modelo/Modelo.java
+++ Tablero/Clasic YuGiOh!/src/modelo/Modelo.java
@@ -1,27 +1,6 @@
 package modelo;
-
-import java.awt.Color;
-import java.awt.Image;
-import java.util.ArrayList;
-
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
-import controlador.TableroController;
-
 public class Modelo {
-	private TableroController controlador;
 
 	
 	
-//Devuelvo la imagen de la carta, gracias a que el objeto carta tiene una ruta de archivo.
-	public JLabel generoImagenCarta(Carta carta) {
-		java.net.URL urlCarta = getClass().getResource(carta.getPathImagen());
-		ImageIcon iconURL = new ImageIcon(
-				new ImageIcon(urlCarta).getImage().getScaledInstance(150, 90, Image.SCALE_DEFAULT));
-		System.out.println("envio un label al controlador");
-		return new JLabel(iconURL);
-
-	}
-
 }
