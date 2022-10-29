93ab6b6d0376b1f0753181514edc6592370379f6
maty-romero <110309525+maty-romero@users.noreply.github.com>
2022-10-29 17:35:36 -0300

    update

--- Tablero/Clasic YuGiOh!/src/controlador/TableroController.java
+++ Tablero/Clasic YuGiOh!/src/controlador/TableroController.java
@@ -22,12 +22,14 @@ public class TableroController {
 	private Modelo modelo;
 	private ControladorProyeccionCartas c;
 
-	private static Duelista duelistaJugador, duelistaOponente;
-	private static Batalla batallaJugador, batallaOponente; // para los cambios de turno
+	private Duelista duelistaJugador, duelistaOponente;
+	private Batalla batallaJugador, batallaOponente; // para los cambios de turno
+	
 	private HashMap<JPanel, CartaMonstruo> manoMonstruoOponente = new HashMap<JPanel, CartaMonstruo>();
 	private HashMap<JPanel, CartaHechizo> manoHechizoOponente = new HashMap<JPanel, CartaHechizo>();
 	private HashMap<JPanel, CartaMonstruo> manoMonstruoJugador = new HashMap<JPanel, CartaMonstruo>();
 	private HashMap<JPanel, CartaHechizo> manoHechizoJugador = new HashMap<JPanel, CartaHechizo>();
+	
 	private JPanel panelCarta;
 	private VistaTablero vista;
 
@@ -37,42 +39,31 @@ public class TableroController {
 		this.modelo = new Modelo();
 		this.c = new ControladorProyeccionCartas(this.vista, this);
 
-		duelistaJugador = new Duelista("YUGI");
-		duelistaOponente = new Duelista("KIRA");
+		this.duelistaJugador = new Duelista("YUGI");
+		this.duelistaOponente = new Duelista("KIRA");
+		
 		batallaJugador = new Batalla(duelistaJugador, duelistaOponente);
 		batallaOponente = new Batalla(duelistaOponente, duelistaJugador);
-
-		/// XXXXXXXXXXXXXXX PRUEBA XXXXXXXXXXX DEBERÌA TRAER CARTAS DESDE EL DECK DEL
-		/// DUELISTA, UNA VEZ LO TENGA CARGADO ASÌ NO GENERO DATOS EN ESTE CONTROLADOR (CON BASE DATOS)
-		ArrayList<CartaMonstruo> monstruosOponente = duelistaOponente.getMano().getManoMonstruos();
-		ArrayList<CartaMonstruo> monstruosJugador = duelistaJugador.getMano().getManoMonstruos();
-		ArrayList<CartaHechizo> hechizosJugador = duelistaJugador.getMano().getManoHechizos();
-		ArrayList<CartaHechizo> hechizosOponente = duelistaOponente.getMano().getManoHechizos();
-		monstruosJugador.add(new CartaMonstruo(1, "hola", "jaja", "/boca_abajo_default/boca_abajo.jpg", 1, 1));
-		monstruosJugador.add(new CartaMonstruo(1, "hola", "jaja", "/cartas/dark_magician.jpg", 1, 1));
-		monstruosJugador.add(new CartaMonstruo(1, "hola", "jaja", "/cartas/dark_magician.jpg", 1, 1));
-		monstruosJugador.add(new CartaMonstruo(1, "hola", "jaja", "/IconoPersonajes/yugi_moto.jpg", 1, 1));
-
-		monstruosOponente.add(new CartaMonstruo(1, "hola", "jaja", "/cartas/cambio_de_fidelidad.jpg", 1, 1));
-		monstruosOponente.add(new CartaMonstruo(1, "hola", "jaja", "/boca_abajo_default/boca_abajo.jpg", 1, 1));
-		monstruosOponente.add(new CartaMonstruo(1, "hola", "jaja", "/cartas/cambio_de_fidelidad.jpg", 1, 1));
-
-		hechizosJugador.add(new CartaHechizo(1, "hola", "jaja", "/hechizos/tifon_espacio_mistico.jpg"));
-		hechizosJugador.add(new CartaHechizo(1, "hola", "jaja", "/hechizos/monstruo_renacido.jpg"));
-		hechizosOponente.add(new CartaHechizo(1, "hola", "jaja", "/hechizos/monstruo_renacido.jpg"));
-		hechizosOponente.add(new CartaHechizo(1, "hola", "jaja", "/hechizos/tifon_espacio_mistico.jpg"));
-
-////XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX///////////////
-
+		
+		//se roban cartas para probar. 
+		duelistaJugador.robarCarta();
+		duelistaJugador.robarCarta();
+		duelistaOponente.robarCarta();
+		duelistaOponente.robarCarta();
+		
 		try {
+			//setteo monstruos usando HashMap
 			this.setManoMonstruoOponente(
-					this.envioImagenesManoMonstruoVista(monstruosOponente, this.vista.getManoBot()));
+					this.envioImagenesManoMonstruoVista(duelistaOponente.getMano().getManoMonstruos(), this.vista.getManoBot()));
 			Thread.sleep(330);
 			this.setManoMonstruoJugador(
-					this.envioImagenesManoMonstruoVista(monstruosJugador, this.vista.getManoJugador()));
-			this.setManoHechizoOponente(this.envioImagenesManoHechizoVista(hechizosOponente, this.vista.getManoBot()));
+					this.envioImagenesManoMonstruoVista(duelistaJugador.getMano().getManoMonstruos(), this.vista.getManoJugador()));
+			
+			//setteo monstruos usando HashMap
+			this.setManoHechizoOponente(
+					this.envioImagenesManoHechizoVista(duelistaOponente.getMano().getManoHechizos(), this.vista.getManoBot()));
 			this.setManoHechizoJugador(
-					this.envioImagenesManoHechizoVista(hechizosJugador, this.vista.getManoJugador()));
+					this.envioImagenesManoHechizoVista(duelistaJugador.getMano().getManoHechizos(), this.vista.getManoJugador()));
 
 			this.vista.getTablero().setVisible(true); // Actualizo el JFrame
 		} catch (InterruptedException e) {
@@ -80,8 +71,15 @@ public class TableroController {
 		}
 	}
 
-//traigo las cartas cargadas con informacion desde base de datos, y las asocio con un panel que creo a partir traer una imagen desde el modelo y cargar esa imagen en el panel en la vista. Luego traigo ese panel y lo asocio a la carta de la cual saqué esa imagen
-//me sirve tener metodos separados; de monstruo y hechizo, porque luego en el hash de monstruos solo van a ir monstruos, y no cartas genericas del tipo Carta
+	/*
+	 * traigo las cartas cargadas con informacion desde base de datos, y las asocio con un panel que creo a partir traer 
+	 * una imagen desde el modelo y cargar esa imagen en el panel en la vista. Luego traigo ese panel y lo asocio a la 
+	 * carta de la cual saqué esa imagen
+	 * 
+	 * me sirve tener metodos separados; de monstruo y hechizo, porque luego en el hash de monstruos solo van a ir 
+	 * monstruos, y no cartas genericas del tipo Carta
+	 */
+	
 	public HashMap<JPanel, CartaMonstruo> envioImagenesManoMonstruoVista(ArrayList<CartaMonstruo> monstruos,
 			JPanel mano) {
 		HashMap<JPanel, CartaMonstruo> hashAuxiliar = new HashMap<JPanel, CartaMonstruo>();
@@ -137,7 +135,9 @@ public class TableroController {
 			duelistaJugador.robarCarta();
 			duelistaOponente.robarCarta();
 		}
-
+		
+		
+		
 		// mientras ninguno pierda, sigue la partida.
 		while (duelistaJugador.getGanador() && duelistaOponente.getGanador()) {
 
