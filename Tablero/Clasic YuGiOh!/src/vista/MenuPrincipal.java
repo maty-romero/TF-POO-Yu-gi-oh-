8f1c43dfa64a2813f83c9d4afae7aac316ea4ede
maty-romero <110309525+maty-romero@users.noreply.github.com>
2022-11-09 15:21:30 -0300

    cambios

--- Tablero/Clasic YuGiOh!/src/vista/MenuPrincipal.java
+++ Tablero/Clasic YuGiOh!/src/vista/MenuPrincipal.java
@@ -1,104 +1,80 @@
 package vista;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-<<<<<<< Updated upstream
-=======
+import java.awt.Font;
 import java.awt.Image;
-import java.awt.Window;
->>>>>>> Stashed changes
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import controlador.TableroController;
-
-import javax.swing.JButton;
-import java.awt.Font;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class MenuPrincipal extends JFrame {
 
 	private JPanel contentPane;
-<<<<<<< Updated upstream
 
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MenuPrincipal frame = new MenuPrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-=======
 	private JLabel fondoMenuPpal;
 	private JButton btnSalir;
 	private JButton btnNuevaPartida;
->>>>>>> Stashed changes
-
+	private JButton btnOpciones;
 
 	public MenuPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 715, 712);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-<<<<<<< Updated upstream
-	}
-
-=======
 		contentPane.setLayout(null);
-		
-		//Fondo Menu Principal
-		java.net.URL fondoURL = getClass().getResource("/fondo_tablero/FondoMenuPrincipal.jpg"); // 
+		setContentPane(contentPane);
+
+		// Fondo Menu Principal
+		java.net.URL fondoURL = getClass().getResource("/fondo_tablero/FondoMenuPrincipal.jpg"); //
 		ImageIcon iconFondoTablero = new ImageIcon(
 				new ImageIcon(fondoURL).getImage().getScaledInstance(700, 700, Image.SCALE_DEFAULT));
 		
-		btnSalir = new JButton("Salir");
-		btnSalir.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnSalir.setBounds(300, 397, 119, 34);
-		contentPane.add(btnSalir);
-		
+		this.fondoMenuPpal = new JLabel(iconFondoTablero);
+		this.fondoMenuPpal.setBounds(0, 0, 698, 679);
+		contentPane.add(fondoMenuPpal);
+
+		//Boton Nueva Partida 
 		btnNuevaPartida = new JButton("Nueva Partida");
 		btnNuevaPartida.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnNuevaPartida.setBounds(300, 277, 119, 34);
 		contentPane.add(btnNuevaPartida);
 
+		//Boton Opciones
+		btnOpciones = new JButton("Opciones");
+		btnOpciones.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnOpciones.setBounds(300, 337, 119, 34);
+		contentPane.add(btnOpciones);
+
+		//Boton Salir
+		btnSalir = new JButton("Salir");
+		btnSalir.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnSalir.setBounds(300, 397, 119, 34);
+		contentPane.add(btnSalir);
 		
-		this.fondoMenuPpal = new JLabel(iconFondoTablero);
-		this.fondoMenuPpal.setBounds(0, 0, 698, 679);
 		
-		contentPane.add(fondoMenuPpal);
+		
 	}
 
+	
 
 	public JButton getBtnSalir() {
 		return btnSalir;
 	}
 
-
 	public void setBtnSalir(JButton btnSalir) {
 		this.btnSalir = btnSalir;
 	}
-
 
 	public JButton getBtnNuevaPartida() {
 		return btnNuevaPartida;
 	}
 
-
 	public void setBtnNuevaPartida(JButton btnNuevaPartida) {
 		this.btnNuevaPartida = btnNuevaPartida;
 	}
-	
-	
->>>>>>> Stashed changes
+
 }
